0a247cad453c816c90df8174ff31a7d479a9b76d
unknown <o.adrian.rodrigues@gmail.com>
2023-06-06 16:09:44 -0300

    Solution with Function class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
@@ -20,14 +21,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		double factor = 1.1;
-	
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
-		list.forEach(p -> p.setPrice(p.getPrice()*factor));
-		
-		list.forEach(System.out::println);
-		
-
+		names.forEach(System.out::println);
 	}
 
 }
