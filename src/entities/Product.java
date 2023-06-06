7ebb35dd56a63147052081e040dec27dc0022422
unknown <o.adrian.rodrigues@gmail.com>
2023-06-06 15:58:10 -0300

    Solution with static method

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -26,6 +26,10 @@ public class Product {
 		this.price = price;
 	}
 	
+	public static void staticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
+	}
+	
 	public static boolean staticProductPredicate(Product p) {
 		return p.getPrice() >= 100.0;
 	}
