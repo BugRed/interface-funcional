5df11a9a13fd3b37b2ec2a6b12a514857d0d8db2
unknown <o.adrian.rodrigues@gmail.com>
2023-06-06 16:12:40 -0300

    Solution with static method

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -29,6 +29,10 @@ public class Product {
 	public static void staticPriceUpdate(Product p) {
 		p.setPrice(p.getPrice() * 1.1);
 	}
+	
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
+	}
 	public void nonStaticPriceUpdate() {
 		price = price * 1.1;
 	}
